789e7189ca11f9cb1847fa26f47e750249c25a4b
Leonardo <leonardozangulo@gmail.com>
2018-02-24 12:34:55 -0600

    Base de datos planes

---
+++ db/plnes.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: banco_patito
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `cat_planes`
+--
+
+LOCK TABLES `cat_planes` WRITE;
+/*!40000 ALTER TABLE `cat_planes` DISABLE KEYS */;
+INSERT INTO `cat_planes` VALUES (1,'Plan 100','Con los Planes Telcel Max Mixto utiliza tus megas, minutos y mensajes incluidos en México, Estados Unidos y Canadá. Todo bajo tu control, ya que si te acabas los minutos, mensajes o megas, puedes recargar saldo. *Los minutos, SMS y Megabytes expresados en el Plan Telcel Max 1000 (Plazo libre) ya incluyen los Minutos, SMS y MB adicionales otorgados por promoción.'),(2,'Plan 150','Con los Planes Telcel Max utiliza tus megas, minutos y mensajes incluidos en México, Estados Unidos y Canadá. Ahora tú eliges la mejor forma de comunicarte. *Los minutos, SMS y Megabytes expresados en el Plan Telcel Max 1000 (Plazo libre) ya incluyen los Minutos, SMS y MB adicionales otorgados por promoción.'),(3,'Plan 700','Con los Planes Internet Telcel Mixto utiliza los MB incluidos en tu Plan en México, Estados Unidos y Canadá. Además navega sin costo en WhatsApp, Facebook, Messenger, Twitter, Instagram, Snapchat, y Uber.'),(4,'Plan 600','Con los Planes Internet Telcel utiliza los MB incluidos en tu Plan en México, Estados Unidos y Canadá. Además navega sin costo en WhatsApp, Facebook, Messenger, Twitter, Instagram, Snapchat, y Uber.'),(5,'Plan 500','Con los Planes Telcel Max Sin Límite Mixtos, tienes Minutos y Mensajes de Texto (SMS) sin límite en México, E.U.A. y Canadá. Todo bajo tu control, si te acabas tus Megas, puedes recargar saldo y continuar navegando');
+/*!40000 ALTER TABLE `cat_planes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-24 12:33:42
